4ba22a35c3e1a553d314bec504e9245b5ab69b58
JBM <bumkuns@gmail.com>
2017-11-17 15:14:37 +0900

    register works login doesn't works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import {AngularFireModule} from 'angularfire2'
+import { AngularFireModule } from 'angularfire2'
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { FIREBASE_CONFIG } from "./app.firebase.config";
-import { AngularFireAuthModule} from "angularfire2/auth";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
     LoginPage,
   ],
   imports: [
@@ -26,7 +24,6 @@ import { AngularFireAuthModule} from "angularfire2/auth";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
     LoginPage,
   ],
   providers: [
